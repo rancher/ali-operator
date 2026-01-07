3bc5a3941ed03024fa38579d4ae9d468e089db2a
Swastik Gour <swastik.gour@suse.com>
2026-01-07 17:32:14 +0530

    [release/v2.13] added mechanism to change the state of autoscaling (#80)
    * added mechanism to change the state of autoscaling
    
    Signed-off-by: swastik959 <Sswastik959@gmail.com>
    
    * updated ali-config test
    
    Signed-off-by: swastik959 <Sswastik959@gmail.com>
    
    * Optimized logic to be done in 1 function call
    
    Signed-off-by: swastik959 <Sswastik959@gmail.com>
    
    ---------
    
    Signed-off-by: swastik959 <Sswastik959@gmail.com>

--- controller/ali-cluster-config-handler.go
+++ controller/ali-cluster-config-handler.go
@@ -588,39 +588,48 @@ func (h *Handler) handleUpdateNodePools(
 ) (bool, error) {
 	var failed []string
 	changed := false
+
 	for _, np := range updateQueue {
 		unp, ok := upstream[np.NodePoolID]
 		if !ok {
 			continue
 		}
-		if tea.BoolValue(unp.EnableAutoScaling) != tea.BoolValue(np.EnableAutoScaling) {
-			logrus.Warnf("Skipping update for nodepool [%s], scaling configuration mismatch in cluster [%s]", unp.Name, configSpec.ClusterName)
-			continue
-		}
-		if np.EnableAutoScaling != nil && np.MinInstances != nil && np.MaxInstances != nil &&
-			unp.EnableAutoScaling != nil && tea.BoolValue(unp.EnableAutoScaling) && tea.BoolValue(np.EnableAutoScaling) &&
-			(tea.Int64Value(np.MaxInstances) != tea.Int64Value(unp.MaxInstances) ||
-				tea.Int64Value(np.MinInstances) != tea.Int64Value(unp.MinInstances)) {
+		if tea.BoolValue(np.EnableAutoScaling) {
+			if !tea.BoolValue(unp.EnableAutoScaling) ||
+				(tea.Int64Value(np.MaxInstances) != tea.Int64Value(unp.MaxInstances)) ||
+				(tea.Int64Value(np.MinInstances) != tea.Int64Value(unp.MinInstances)) {
 
-			logrus.Infof("Updating nodepool [%s] in cluster [%s] from maxInstances:[%d] minInstance: [%d] to maxInstances:[%d] minInstance: [%d]",
-				np.Name, configSpec.ClusterName, tea.Int64Value(unp.MaxInstances), tea.Int64Value(unp.MinInstances), tea.Int64Value(np.MaxInstances), tea.Int64Value(np.MinInstances))
-			err := alibaba.UpdateNodePoolAutoScalingConfig(ctx, h.alibabaClients.clustersClient, configSpec.ClusterID, np.NodePoolID, np.MaxInstances, np.MinInstances)
-			if err != nil {
-				failed = append(failed, fmt.Sprintf("nodepool %s autoscaling update error: %s", np.Name, err.Error()))
-				continue
+				logrus.Infof("Updating nodepool [%s] AutoScaling -> TRUE. Limits: Min [%d] Max [%d]",
+					np.Name, tea.Int64Value(np.MinInstances), tea.Int64Value(np.MaxInstances))
+
+				err := alibaba.UpdateNodePoolConfig(ctx, h.alibabaClients.clustersClient, configSpec.ClusterID, np.NodePoolID, true, np.MaxInstances, np.MinInstances, nil)
+				if err != nil {
+					failed = append(failed, fmt.Sprintf("nodepool %s autoscaling enable error: %s", np.Name, err.Error()))
+					continue
+				}
+				changed = true
 			}
-			changed = true
-		} else if np.DesiredSize != nil && unp.DesiredSize != nil && tea.Int64Value(np.DesiredSize) != tea.Int64Value(unp.DesiredSize) {
-			logrus.Infof("Updating desired size for nodepool [%s] in cluster [%s] from %d to %d",
-				np.Name, configSpec.ClusterName, tea.Int64Value(unp.DesiredSize), tea.Int64Value(np.DesiredSize))
-			err := alibaba.UpdateNodePoolDesiredSize(ctx, h.alibabaClients.clustersClient, configSpec.ClusterID, np.NodePoolID, np.DesiredSize)
-			if err != nil {
-				failed = append(failed, fmt.Sprintf("nodepool %s desired size update error: %s", np.Name, err.Error()))
-				continue
+		} else {
+			shouldUpdate := false
+			if tea.BoolValue(unp.EnableAutoScaling) {
+				shouldUpdate = true
+				logrus.Infof("Disabling AutoScaling for nodepool [%s] and setting Fixed Size: %d", np.Name, tea.Int64Value(np.DesiredSize))
+			} else if np.DesiredSize != nil && unp.DesiredSize != nil && tea.Int64Value(np.DesiredSize) != tea.Int64Value(unp.DesiredSize) {
+				shouldUpdate = true
+				logrus.Infof("Updating desired size for nodepool [%s] from %d to %d",
+					np.Name, tea.Int64Value(unp.DesiredSize), tea.Int64Value(np.DesiredSize))
 			}
-			changed = true
+			if shouldUpdate {
+				err := alibaba.UpdateNodePoolConfig(ctx, h.alibabaClients.clustersClient, configSpec.ClusterID, np.NodePoolID, false, nil, nil, np.DesiredSize)
+				if err != nil {
+					failed = append(failed, fmt.Sprintf("nodepool %s update error: %s", np.Name, err.Error()))
+					continue
+				}
+				changed = true
+			}
 		}
 	}
+
 	if len(failed) > 0 {
 		return changed, fmt.Errorf("%s", strings.Join(failed, ";"))
 	}
@@ -660,6 +669,10 @@ func needsUpdate(desired []aliv1.AliNodePool, upstream map[string]aliv1.AliNodeP
 		unp, ok := upstream[np.NodePoolID]
 		if !ok {
 			continue
+		}
+
+		if tea.BoolValue(np.EnableAutoScaling) != tea.BoolValue(unp.EnableAutoScaling) {
+			return true
 		}
 		// AutoScaling mismatch
 		if np.EnableAutoScaling != nil && unp.EnableAutoScaling != nil {
